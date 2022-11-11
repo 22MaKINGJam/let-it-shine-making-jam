41ed9b464a34490c6b014701a1078feb038a25d5
suabae <bsa0322@ewhain.net>
2022-11-11 22:31:09 +0900

    init

---
+++ Assets/Scripts/Sound/BackgroundSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSound : MonoBehaviour
+{
+    public static AudioSource backmusic;
+
+    private void Awake()
+    {
+        backmusic = GetComponent<AudioSource>();
+        if (backmusic.isPlaying) return;
+        else
+        {
+            backmusic.Play();
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Sound/ButtonSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public static ButtonSound _buttonInstance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _buttonInstance = this;
+    }
+
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>(); //GameManager 오브젝트
+    }
+}
