ca1a1169f0177ae56488e3e41e2fbf3ab6ecabe1
suabae <bsa0322@ewhain.net>
2022-11-12 01:39:22 +0900

    [ADD] create random item & character 2th jump

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // 0: cookie, 1: candy, 2: cheeze, 3: gift 
+    public Sprite[] sprites;
+    private int number;
+    private float x, y;
+
+    // player item trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            ItemEffect(number);
+        }
+        else if(collision.gameObject.tag == "Item")
+        {
+            OnDeleteObject();
+        }
+    }
+
+    public void ItemEffect(int idx)
+    {
+        switch (idx)
+        {
+            case 0:
+                // 속도 n만큼 증가
+                OnDeleteObject();
+                break;
+            case 1:
+                // 점프 길이 n만큼 증가 (점프 force 증가)
+                OnDeleteObject();
+                break;
+            case 2:
+                // 점수 up
+                OnDeleteObject();
+                break;
+            case 3:
+                // 랜덤으로 아이템 나오게
+                // 약간 선물상자 뜯어지는 애니메이션
+                // 새로운 아이템
+                StartCoroutine(NewItem());
+                gameObject.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+                break;
+        }
+    }
+    
+
+    IEnumerator NewItem()
+    {
+        yield return new WaitForSeconds(1f);
+
+        GameObject temp = GameObject.Find("ItemManager").GetComponent<ItemManager>().CreateItemPosition(x, y, sprites.Length - 1);
+        Item newItem = temp.GetComponent<Item>();
+
+        // 바로 사용 + 사라짐
+        yield return new WaitForSeconds(1f);
+        newItem.ItemEffect(newItem.GetNumber());
+        OnDeleteObject();
+    }
+
+    public float GetX()
+    {
+        return x;
+    }
+
+    public float GetY()
+    {
+        return y;
+    }
+
+    public int GetNumber()
+    {
+        return number;
+    }
+
+    public void SetSprite(int idx)
+    {
+        number = idx;
+        x = transform.position.x;
+        y = transform.position.y;
+        GetComponent<SpriteRenderer>().sprite = sprites[idx];
+    }
+
+    public void OnDeleteObject()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Item/ItemManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public GameObject itemPrefab, parent;
+
+    public int itemCount;
+    public float minX, maxX, minY, maxY;
+    public float minIntervalY, maxIntervalY;
+
+    private Item[] items;
+    private int itemNumber;
+
+    private float currentY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentY = minY;
+        itemNumber = itemPrefab.GetComponent<Item>().sprites.Length;
+        CreateItemInit();
+    }
+
+    void CreateItemInit()
+    {
+        items = new Item[itemCount];
+        for(int i = 0; i < itemCount; i++)
+        {
+            float x = Random.Range(minX, maxX);
+            float y = currentY + Random.Range(minIntervalY, maxIntervalY);
+            currentY = y;
+
+            items[i] = CreateItemPosition(x, y, itemNumber).GetComponent<Item>();
+        }
+    }
+
+    public GameObject CreateItemPosition(float x, float y, int range)
+    {
+        Vector2 creatingPoint = new Vector2(x, y);
+        GameObject temp = null;
+        while(temp == null)
+        {
+            temp = Instantiate(itemPrefab, creatingPoint, Quaternion.identity);
+        }
+        temp.transform.parent = parent.transform;
+
+        temp.GetComponent<Item>().SetSprite(Random.Range(0, range));
+        return temp;
+    }
+}
