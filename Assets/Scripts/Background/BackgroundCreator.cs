fb27450d4072411aca7c5c798e5b619b6b964fb0
sforseohn <sforseohn@ewhain.net>
2022-11-13 00:34:56 +0900

    배경 설정

---
+++ Assets/Scripts/Background/BackgroundCreator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCreator : MonoBehaviour
+{
+    public GameObject middle1;
+    public GameObject middle2;
+    public GameObject first;
+    public GameObject end;
+    public int backgroundNumbers = 20;
+
+    private int backgroundLength;
+    private int y = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBackground();
+    }
+
+   private void CreateBackground()
+    {
+        backgroundLength = backgroundNumbers * 10 + 20; // 중간개수*10 + 처음 + 마지막.
+
+        y = 0;
+        y = SetPosition(first, y);
+        for (int i = 0; i < backgroundNumbers; i++)
+        {
+            if (i % 2 == 0)
+            {
+                //SetPosition(middle1, y);
+                y = SetPosition(middle1, y);
+            }
+            else
+            {
+                // middle2생성
+                SetPosition(middle2, y);
+                y = SetPosition(middle2, y);
+            }
+        }
+            SetPosition(end, y);
+
+    }
+
+    public int SetPosition(GameObject background, int y)
+    {
+        Vector3 creatingPoint = new Vector3(0, y,0);
+        y += 10;
+        Instantiate(background, creatingPoint, Quaternion.identity);
+        return y;
+    }
+}
--- Assets/Scripts/Platforms/PlatformGenerator.cs
+++ Assets/Scripts/Platforms/PlatformGenerator.cs
@@ -11,7 +11,6 @@ public class PlatformGenerator : MonoBehaviour
     public float levelWidth = 3f;
     public float minY = .7f;
     public float maxY = 1.5f;
-    float playerPos;
 
     private float camerapos;
     Vector3 spawnPosition;
@@ -25,7 +24,7 @@ public class PlatformGenerator : MonoBehaviour
         for (int i = 0; i < numberOfPlatforms; i++)
         {
             spawnPosition.x = Random.Range(- 2, 2);
-            spawnPosition.y += Random.Range(minY, maxY); //사이의 값만 생성.
+            spawnPosition.y += Random.Range(minY, maxY); 
             temp = Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
             temp.transform.parent = this.gameObject.transform;
         }
