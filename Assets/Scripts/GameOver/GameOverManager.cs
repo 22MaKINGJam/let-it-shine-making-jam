f704f8b63370f47d057f54e15d4111acb217eebf
sforseohn <sforseohn@ewhain.net>
2022-11-13 09:16:53 +0900

    가시 게임오버 설정

--- Assets/Scripts/GameOver/GameOverManager.cs
+++ Assets/Scripts/GameOver/GameOverManager.cs
@@ -13,7 +13,7 @@ public class GameOverManager : MonoBehaviour
     private float height;
     private void Awake()
     {
-        gameover = () => { GameOver(); };
+        gameover = () => { gameOver(); };
         splinter = GameObject.FindWithTag("Splinter");
         height = this.GetComponent<BoxCollider2D>().bounds.size.y;
     }
@@ -28,24 +28,13 @@ public class GameOverManager : MonoBehaviour
         cameraBottom = mainCamera.transform.position.y - 5;
         if (this.transform.position.y - height/2 < cameraBottom)
         {
-            GameOver();
+            gameOver();
         }
     }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    CheckIfSplinter(collision);
-    //}
 
-    //private void CheckIfSplinter(Collider2D collision) // 2. 플레이어가 가시에 닿으면 죽음.
-    //{
-    //    if (collision.gameObject.Equals(splinter))
-    //    {
-    //        GameOver();
-    //    }
-    //}
 
-    public void GameOver()
+    public void gameOver()
     {
         Debug.Log("게임오버");
         // 게임오버되면? 메인으로 씬 전환? 기획애니메이션으로 씬 전환?
