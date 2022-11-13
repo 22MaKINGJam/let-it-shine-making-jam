e370931106a5fe13090246081c3d10541a7a257c
sforseohn <sforseohn@ewhain.net>
2022-11-13 16:16:45 +0900

    씬넘어가게

--- Assets/Scripts/StageClear/Stage2Clear.cs
+++ Assets/Scripts/StageClear/Stage2Clear.cs
@@ -1,21 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Stage1Clear : MonoBehaviour
+public class Stage2Clear : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
             Debug.Log("스테이지 원 클리어!");
-            GameObject.Find("SceneChange").GetComponent<SceneChange>().OnLoadStageTwoScene();
+            GameObject.Find("SceneChange").GetComponent<SceneChange>().OnLoadEndScene();
         }
         else if (collision.gameObject.tag.Equals("Platform"))
         {
             Destroy(collision.gameObject);
         }
     }
-    
 }
