236268a42313bcdfd25bb1749dc642ee3e578af0
sforseohn <100199530+sforseohn@users.noreply.github.com>
2022-11-21 22:50:51 +0900

    heart up when cheese

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -20,4 +20,14 @@ public class LifeManager : MonoBehaviour
             lifeObj.GetComponent<Image>().sprite = lifes[GameSaveData.life];
         }
     }
+
+    public void LifeUp()
+    {
+        if (GameSaveData.life == 1 || GameSaveData.life == 2) // 0 또는 3이면 추가 X
+        {
+            GameSaveData.life++;
+            Debug.Log("하트 하나 추가!");
+            lifeObj.GetComponent<Image>().sprite = lifes[GameSaveData.life];
+        }
+    }
 }
